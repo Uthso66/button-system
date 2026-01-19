f09d85238e1b8da8fcdb5d0efe353b81f9f7dafa
uthso66 <tarikulislam.uthso.3966@gmail.com>
2026-01-19 23:03:17 +0600

    fix: 🛠️ resolve hydration error by adding 'use client' directive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+import Button from "./components/Button";
 export default function Home() {
   return (
     <main className="min-h-screen p-8 bg-linear-to-br from-gray-50 to-gray-100">
@@ -7,6 +9,11 @@ export default function Home() {
       <p className="text-gray-600 mb-12">
         Building a reusable button library with 8 variants
       </p>
+
+      <div className="space-y-4">
+        <Button>Default Button</Button>
+      <Button onClick={() => alert("Clicked")}>⚡ Click Me!</Button>
+      </div>
     </main>
   );
 }
